4e7cd91fab8cf91a927502ac255b49f87d7a869e
sukmkms <9kyokumekemuso@gmail.com>
2024-08-09 12:47:05 +0900

    修正依頼に基づく修正案

--- webnazo-front/src/components/Room/phase1/Phase1.tsx
+++ webnazo-front/src/components/Room/phase1/Phase1.tsx
@@ -10,8 +10,8 @@ const Phase1 = () => {
   const { proceed } = useProceed()
 
   return (
-    <div className="bg-yumekawa bg-cover">
-      <div className="font-pop mx-auto w-3/5 bg-cyan-200 pb-52 pt-7">
+    <div className="bg-yumekawa bg-white/40 bg-cover bg-blend-color">
+      <div className="font-pop mx-auto w-3/5 border-2 border-solid border-fuchsia-200 bg-gradient-to-t from-orange-200 via-lime-300 to-emerald-200 pb-52 pt-7">
         <h1>
           <img
             src={rankmatchLogo}
@@ -19,81 +19,80 @@ const Phase1 = () => {
             className="animate-float mx-auto mb-5 w-1/2"
           />
         </h1>
-        <div className="mx-auto w-11/12 border-2 border-solid border-stone-600 bg-white px-14 py-7">
-          <h2 className="pb-5 text-center text-6xl tracking-widest">
-            このサイトは？
-          </h2>
-          <p className="bg-teal-100 p-8 text-left text-2xl">
-            このサイトでは毎週所定の時間に「
-            <span className="text-red-500">ナゾトキランクマッチ</span>
-            」が開催される！
-            <br />
-            皆で一斉に同じ謎解き問題に参加して、解くまでの時間を競うのだ！解くのが早ければ早いほどレーティングが上がっていくぞ！
-            <br />
-            <span className="text-red-500">
-              全国の謎解きマニアたちと勝負せよ！
-            </span>
-          </p>
-          <p className="mb-5 mt-14 text-center text-3xl font-extrabold">
-            次回のゲームは<span className="text-red-500">8月1日21時</span>
-            からスタート！
-          </p>
-          <div className="text-center">
-            <button
-              className="w-full border-2 border-solid border-stone-600 bg-zinc-200 px-3 py-7 text-center text-7xl"
-              onClick={() => proceed(2)}
-            >
-              ゲームに参加する
-            </button>
-          </div>
-          <div className="mb-20 mt-16 bg-pink-100 p-5">
-            <h3 className="mb-7 text-6xl">前回の成績優秀者</h3>
-            <ul className="leading-loose">
-              <li className="flex text-6xl">
-                <img src={rank1} alt="一位" className="w-48" />
-                <span className="mb-10 mt-auto">知的生命体YOSHIO さん</span>
-              </li>
-              <li className="mb-7 flex pl-8 text-5xl">
-                <img src={rank2} alt="二位" className="w-32" />
-                <span className="mb-7 ml-3 mt-auto">北亨夏高校クイ研 さん</span>
-              </li>
-              <li className="flex pl-14 text-4xl">
-                <img src={rank3} alt="三位" className="w-20" />
-                <span className="mb-4 ml-7 mt-auto">ゆ（＠ω＠） さん</span>
-              </li>
-            </ul>
-          </div>
-          <div className="mb-20 bg-red-50 p-5">
-            <img src={pyramid} alt="ピラミッド君" className="mx-auto mb-10" />
-            <h3 className="text-center text-5xl">ピラミッド君</h3>
-            <p className="mt-8 text-2xl">
-              このサイトのマスコットキャラクター。謎で頂点を目指すユーザーの皆を応援している。可愛い外見で毒のあることを言うという自己認識を持っているが、そもそも自分が可愛くないことに気づいていない。
+        <div className="mx-auto w-11/12 rounded-2xl border border-solid border-fuchsia-200 bg-white px-2 py-2">
+          <div className="rounded-2xl border-2 border-dashed border-fuchsia-200 px-12 py-5">
+            <h2 className="pb-5 text-center text-6xl tracking-widest">
+              このサイトは？
+            </h2>
+            <p className="bg-teal-100 p-8 text-left text-2xl">
+              このサイトでは毎週所定の時間に「
+              <span className="text-red-500">ナゾトキランクマッチ</span>
+              」が開催される！
               <br />
-              オカルトが大の苦手であり、心霊スポットなどお化けが出そうなところからはすぐに逃げ出してしまう。
+              皆で一斉に同じ謎解き問題に参加して、解くまでの時間を競うのだ！解くのが早ければ早いほどレーティングが上がっていくぞ！
               <br />
-              そのためピラミッドが王族の墓として作られたという通説を真っ赤なデマであるとして強く否定している。
+              <span className="text-red-500">
+                全国の謎解きマニアたちと勝負せよ！
+              </span>
             </p>
-          </div>
-          <div className="bg-orange-100 p-5">
-            <h3 className="mb-10 text-center text-5xl">注意事項</h3>
-            <ul className="text-2xl">
-              {/*
-              デフォルトでリストの点がなかったため、
-              list-discで点をつけている
-              （検証したブラウザはEdgeとChrome）
-              */}
-              <li className="list-inside list-disc">
-                シーズン中は謎の回答をSNSなどでネタバレしないようにしてください。
-              </li>
-              <br />
-              <li className="list-inside list-disc">
-                このサイトは同時に多くのユーザーが閲覧しています。過度な更新を行うとサーバーに過剰な負荷がかかる恐れがあるため、ページが表示されない時にたくさん更新はしないでください。
-              </li>
-              <br />
-              <li className="list-inside list-disc">
-                ソースコードを閲覧して解く行為はお控えください。
-              </li>
-            </ul>
+            <p className="mb-5 mt-14 text-center text-3xl font-extrabold">
+              次回のゲームは<span className="text-red-500">8月1日21時</span>
+              からスタート！
+            </p>
+            <div className="text-center">
+              <button
+                className="w-full border-2 border-solid border-stone-600 bg-zinc-200 px-3 py-7 text-center text-7xl"
+                onClick={() => proceed(2)}
+              >
+                ゲームに参加する
+              </button>
+            </div>
+            <div className="mb-20 mt-16 bg-pink-100 p-12">
+              <h3 className="mb-7 text-6xl">前回の成績優秀者</h3>
+              <ul className="leading-loose">
+                <li className="flex text-6xl">
+                  <img src={rank1} alt="一位" className="w-48" />
+                  <span className="mb-10 mt-auto">知的生命体YOSHIO さん</span>
+                </li>
+                <li className="mb-7 flex pl-8 text-5xl">
+                  <img src={rank2} alt="二位" className="w-32" />
+                  <span className="mb-7 ml-3 mt-auto">
+                    北亨夏高校クイ研 さん
+                  </span>
+                </li>
+                <li className="flex pl-14 text-4xl">
+                  <img src={rank3} alt="三位" className="w-20" />
+                  <span className="mb-4 ml-7 mt-auto">ゆ（＠ω＠） さん</span>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-20 bg-red-50 p-12">
+              <img src={pyramid} alt="ピラミッド君" className="mx-auto mb-10" />
+              <h3 className="text-center text-5xl">ピラミッド君</h3>
+              <p className="mt-8 text-2xl">
+                このサイトのマスコットキャラクター。謎で頂点を目指すユーザーの皆を応援している。可愛い外見で毒のあることを言うという自己認識を持っているが、そもそも自分が可愛くないことに気づいていない。
+                <br />
+                オカルトが大の苦手であり、心霊スポットなどお化けが出そうなところからはすぐに逃げ出してしまう。
+                <br />
+                そのためピラミッドが王族の墓として作られたという通説を真っ赤なデマであるとして強く否定している。
+              </p>
+            </div>
+            <div className="bg-orange-100 p-12">
+              <h3 className="mb-10 text-center text-5xl">注意事項</h3>
+              <ul className="text-2xl">
+                <li className="list-inside list-disc">
+                  シーズン中は謎の回答をSNSなどでネタバレしないようにしてください。
+                </li>
+                <br />
+                <li className="list-inside list-disc">
+                  このサイトは同時に多くのユーザーが閲覧しています。過度な更新を行うとサーバーに過剰な負荷がかかる恐れがあるため、ページが表示されない時にたくさん更新はしないでください。
+                </li>
+                <br />
+                <li className="list-inside list-disc">
+                  ソースコードを閲覧して解く行為はお控えください。
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
