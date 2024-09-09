dcec076cb2133dd06502396412a83af2f716c2a0
substitute1191 <takahiromasuda14@gmail.com>
2024-09-09 12:20:43 +0900

    ホラー広告が出現する

--- webnazo-front/src/components/Room/phase3/Advertisement.tsx
+++ webnazo-front/src/components/Room/phase3/Advertisement.tsx
@@ -1,4 +1,4 @@
-import advSrc from "@/assets/image/広告背景.png"
+import advSrc from "@/assets/image/adv/広告背景1.png"
 import useSE from "@/SoundManager/useSE"
 import windowSE from "@/assets/sound/ビープ音1.mp3"
 import { useEffect } from "react"
@@ -15,8 +15,11 @@ const Advertisement = () => {
   }, [play, stop])
 
   return (
-    <div className="absolute top-[15rem]">
-      <img src={advSrc} alt="" />
+    <div className="absolute top-[15rem] z-10">
+      <div className="absolute right-3 top-[1rem] z-10 w-[1.6rem] border border-slate-100 bg-red-500 text-center shadow-md">
+        ✕
+      </div>
+      <img src={advSrc} alt="" className="z-5 relative" />
     </div>
   )
 }
