55947580e049960d5b4277aa21e420210fc23404
substitute1191 <takahiromasuda14@gmail.com>
2024-09-10 11:09:19 +0900

    phase3のアニメーション周りをリファクタリング

--- webnazo-front/src/components/Room/phase3/Phase3.tsx
+++ webnazo-front/src/components/Room/phase3/Phase3.tsx
@@ -6,71 +6,12 @@ import Phase3BGMProvider from "./Phase3BGMProvider"
 import Advertisement from "./Advertisement/Advertisement"
 import useAnimationState from "./hooks/useAnimationState"
 import FakeCursor from "./Cursor/FakeCursor"
-import { useCallback, useEffect, useMemo, useRef } from "react"
-import useFakeCursor from "./Cursor/useFakeCursor"
-import useTextManager from "./hooks/useTextManager"
+import usePhase3Anim from "./usePhase3Anim"
 
 const Phase3 = () => {
   const { animStep, handleAnimEnd } = usePhase3AnimStep()
-  const { setShowText } = useTextManager()
-  const { isShowAdv, isApproachingCloseBtn } = useAnimationState()
-  const cursorRef = useRef<HTMLImageElement>(null)
-  const { setCursorPos } = useFakeCursor()
-  const animationRef = useRef<number>()
-
-  const warningLines = useMemo(
-    () => ["人の話聞いてる！？", "バチボコにヤバいよ！", "あわわわわわわ"],
-    []
-  )
-
-  const animate = useCallback(() => {
-    const closeBtn = document.getElementById("closeBtn")
-    if (closeBtn === null || cursorRef.current === null) return
-    const cursorPos = cursorRef.current.getBoundingClientRect()
-    const closeBtnPos = closeBtn.getBoundingClientRect()
-
-    const dx = closeBtnPos.x - cursorPos.x
-    const dy = closeBtnPos.y - cursorPos.y
-
-    const speed = 1.5
-    const distance = Math.sqrt(dx * dx + dy * dy)
-
-    if (240 < distance && distance < 250) {
-      setShowText(warningLines[0])
-    }
-
-    if (130 < distance && distance < 150) {
-      setShowText(warningLines[1])
-    }
-
-    if (70 < distance && distance < 80) {
-      setShowText(warningLines[2])
-    }
-
-    if (distance > speed) {
-      const ratio = speed / distance
-      const newX = cursorPos.x + dx * ratio
-      const newY = cursorPos.y + dy * ratio
-      setCursorPos({
-        x: newX,
-        y: newY,
-      })
-      animationRef.current = requestAnimationFrame(animate)
-    } else {
-      setCursorPos(closeBtnPos)
-    }
-  }, [setCursorPos, setShowText, warningLines])
-
-  useEffect(() => {
-    if (isApproachingCloseBtn) {
-      animationRef.current = requestAnimationFrame(animate)
-
-      return () => {
-        if (animationRef.current !== undefined)
-          cancelAnimationFrame(animationRef.current)
-      }
-    }
-  }, [animate, isApproachingCloseBtn, setCursorPos])
+  const { isShowAdv } = useAnimationState()
+  const { cursorRef } = usePhase3Anim()
 
   return (
     <Phase3BGMProvider>
---
+++ webnazo-front/src/components/Room/phase3/usePhase3Anim.ts
@@ -0,0 +1,52 @@
+import { useCallback, useEffect, useMemo, useRef } from "react"
+import useFakeCursor from "./Cursor/useFakeCursor"
+import useTextManager from "./hooks/useTextManager"
+import useAnimationState from "./hooks/useAnimationState"
+import { calculateNewPosition, setWaringLines } from "./phase3AnimUtils"
+
+const usePhase3Anim = () => {
+  const cursorRef = useRef<HTMLImageElement>(null)
+  const { setCursorPos } = useFakeCursor()
+  const { setShowText } = useTextManager()
+  const { isApproachingCloseBtn } = useAnimationState()
+  const animationRef = useRef<number>()
+
+  const warningLines = useMemo(
+    () => [
+      "人の話聞いてる！？",
+      "バチボコにヤバい！",
+      "あわわわわわわわわわわわわ",
+    ],
+    []
+  )
+
+  const animate = useCallback(() => {
+    const closeBtn = document.getElementById("closeBtn")
+    if (closeBtn === null || cursorRef.current === null) return
+    const cursorPos = cursorRef.current.getBoundingClientRect()
+    const closeBtnPos = closeBtn.getBoundingClientRect()
+
+    const { x, y, distance } = calculateNewPosition(cursorPos, closeBtnPos, 1.5)
+
+    const line = setWaringLines(distance, warningLines)
+    if (line !== null) setShowText(line)
+
+    setCursorPos({ x, y })
+    animationRef.current = requestAnimationFrame(animate)
+  }, [setCursorPos, setShowText, warningLines])
+
+  useEffect(() => {
+    if (isApproachingCloseBtn) {
+      animationRef.current = requestAnimationFrame(animate)
+
+      return () => {
+        if (animationRef.current !== undefined)
+          cancelAnimationFrame(animationRef.current)
+      }
+    }
+  }, [animate, isApproachingCloseBtn, setCursorPos])
+
+  return { cursorRef }
+}
+
+export default usePhase3Anim
