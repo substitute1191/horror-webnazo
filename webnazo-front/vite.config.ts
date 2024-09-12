8a91ac72f532d45e81a2edd0d188cd3c9c60e745
substitute1191 <takahiromasuda14@gmail.com>
2024-09-12 22:38:35 +0900

    glslを読み込める設定を追加

--- webnazo-front/vite.config.ts
+++ webnazo-front/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tsconfigPaths from "vite-tsconfig-paths"
 import svgr from "vite-plugin-svgr"
+import glsl from "vite-plugin-glsl"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr({ include: "**/*.svg" })],
+  plugins: [react(), tsconfigPaths(), svgr({ include: "**/*.svg" }), glsl()],
 })
